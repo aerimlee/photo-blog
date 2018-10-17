28afed2bc7c8afdf9ebbb56e409e3b2a7e3546dd
aerimlee <aerim@sysmate.co.kr>
2018-10-17 15:54:48 +0900

    Photo App test _Basic_Admin

--- photos/models.py
+++ photos/models.py
@@ -1,8 +1,11 @@
+from __future__ import unicode_literals
+
 from django.db import models
+
 
 class Photo(models.Model):
     image = models.ImageField()
     filtered_image = models.ImageField()
     content = models.TextField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
-            
+
