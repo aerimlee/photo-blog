8990cf2fa599d33942b28e6cfa01ce83acd64045
aerimlee <aerim@sysmate.co.kr>
2018-10-19 11:33:23 +0900

    Basic code

--- photos/views.py
+++ photos/views.py
@@ -1,13 +1,4 @@
 from django.shortcuts import render
-from .models import Photo
 
-
-def detail(request, pk):
-    photo = Photo.objects.get(pk=pk)
-
-    messages = (
-        '<p>{pk}ss</p>'.format(pk=photo.pk),
-        '<p>ss{url}</p>'.format(url=photo.image.url),
-        '<p><img src="{url}" /></p>'.format(url=photo.image.url),
-    )
-    return HttpResponse('\n'.join(messages))
+def post_list(request):
+    return render(request, 'photos/post_list.html', {})
--- pystagram/urls.py
+++ pystagram/urls.py
@@ -1,13 +1,8 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
-from django.conf import settings
-from django.conf.urls.static import static
-
-
-from photos.views import detail
 
 urlpatterns = [
-
     url(r'^admin/', admin.site.urls),
+    url(r'', include('photos.urls')),
 ]
 
